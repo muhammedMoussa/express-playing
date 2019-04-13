c737b891b364c866dcf929127c75ada55b27c813
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-04-13 04:36:03 +0200

    implement get all and get by id methods

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ const logger = require('./middleware/logger')
 
 const app = express()
 
-app.use(logger)
+// app.use(logger)
 
+/* Get All */
 app.get('/api/members', (req, res) => {
     res.json(data.members)
+})
+
+/* Get One */
+app.get('/api/members/:id', (req, res) => {
+    const found = data.members.some(member => member.id === parseInt(req.params.id))
+
+    if (found) {
+      res.json(data.members.filter(member => member.id === parseInt(req.params.id)))
+    } else {
+      res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
+    }
 })
 
 // SET STATIC FOLDER
