67db3f759f17ad56965e3834d2761929b7fd0582
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-04-13 04:09:07 +0200

    config serve with static data

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const path = require('path')
+const data = require('./data')
 
 const app = express()
+
+app.get('/api/members', (req, res) => {
+    res.json(data.members)
+})
 
 // SET STATIC FOLDER
 app.use(express.static(path.join(__dirname, 'public')))
